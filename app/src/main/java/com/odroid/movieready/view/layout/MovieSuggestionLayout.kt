6894f4a8fa9b79d6c96876ed51f5eaf98accd5b4
pratapnik <nikhil.pratap.singh.581@gmail.com>
2022-06-28 16:37:41 +0530

    Added Pulsating Animation for card

--- app/src/main/java/com/odroid/movieready/view/layout/MovieSuggestionLayout.kt
+++ app/src/main/java/com/odroid/movieready/view/layout/MovieSuggestionLayout.kt
@@ -1,5 +1,6 @@
 package com.odroid.movieready.view.layout
 
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -8,8 +9,10 @@ import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -19,6 +22,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -61,8 +65,21 @@ fun MovieSuggestionCard(
     onNewMovieButtonClick: () -> Unit
 ) {
     Column {
+        val infiniteTransition = rememberInfiniteTransition()
+
+        val scale by infiniteTransition.animateFloat(
+            initialValue = 0.95f,
+            targetValue = 1f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(1000),
+                repeatMode = RepeatMode.Reverse,
+            )
+        )
+
         Card(
-            modifier = modifier.fillMaxWidth(),
+            modifier = modifier
+                .fillMaxWidth()
+                .scale(scale),
             shape = RoundedCornerShape(15.dp),
             elevation = 5.dp
         ) {
@@ -107,7 +124,7 @@ fun MovieSuggestionCard(
                 }
             }
         }
-        Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.height(32.dp))
         ExtendedFloatingActionButton(
             onClick = onNewMovieButtonClick,
             modifier = Modifier
